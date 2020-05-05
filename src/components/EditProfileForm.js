73ff1b44668b1abc1bbc0f38778e7d9d844223c7
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-05 13:47:56 -0700

    add edit form and reusable form and add bootstrap design elements
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

---
+++ src/components/EditProfileForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+
+function EditProfileForm(props) {
+  const { profile } = props;
+
+  function handleEditProfileFormSubmission(event) {
+    event.preventDefault();
+    props.onEditProfile({
+      name: event.target.name.value,
+      bio: event.target.bio.value,
+      projects:event.target.projects.value,
+      skills: event.target.skills.value,
+      id: v4()
+    });
+  }
+  return (
+    <React.Fragment>
+      <ReusableForm
+      formSubmissionHandler={ handleEditProfileFormSubmission }
+      buttonText="Update Profile" />
+    </React.Fragment>
+  );
+}
+
+EditProfileForm.propTypesj = {
+  onEditProfile: PropTypes.func
+}
+
+export default EditProfileForm;
---
+++ src/components/ReusableForm.js
@@ -0,0 +1,61 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Button, Form } from 'react-bootstrap';
+
+
+function ReusableForm(props) {
+  return (
+    
+    <React.Fragment>
+      <Form onSubmit = { props.fromSubmissionHandler }>
+       
+          <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="name">What's your name?</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"/>
+          </InputGroup>
+
+          <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="bio">Tell us about yourself:</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"/>
+          </InputGroup>
+
+          <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="projects">Show us some of your projects!</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"/>
+          </InputGroup>
+
+          <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="skills">What kind of skills do you flex?</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"/>
+          </InputGroup>
+
+          <Button type='submit' variant="info">{props.buttonText}</Button>
+
+      </Form>
+    </React.Fragment>
+  )
+}
+
+ReusableForm.propTypes = {
+  fromSubmissionHandler: PropTypes.func
+};
+
+export default ReusableForm;

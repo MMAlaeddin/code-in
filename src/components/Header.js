57facb2e96de18ecc8085877560828462c86684e
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-05 10:28:28 -0700

    add header and footer
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 
 function Header(props) {
   return (
     <React.Fragment>
+      <Jumbotron>
       <h1>CodeIn</h1>
+      </Jumbotron>
     </React.Fragment>
   );
 }

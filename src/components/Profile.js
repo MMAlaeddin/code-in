1011e358767221e26e4280eab45a59d4d05707c0
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-05 10:21:47 -0700

    add proptypes for profile component
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,10 +9,20 @@ function Profile(props) {
       <h5>{props.bio}</h5>
       <hr/>
       <h3>Projects: </h3>
-      <h5>{props.projects}</h5>
+      <h5><ul>{props.projects}</ul></h5>
       <h3>Skills: </h3>
-      <h5>{props.skills}</h5>
+      <h5><ul>{props.skills}</ul></h5>
       </div>
     </React.Fragment>
-  )
+  );
 }
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  bio: PropTypes.string.isRequired,
+  projects: PropTypes.string,
+  skills: PropTypes.string,
+  whenProfileClicked: PropTypes.func
+};
+
+export default Profile;

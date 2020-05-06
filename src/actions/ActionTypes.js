956f344282951a6cfdfaa81f2921fac3c406d8d5
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-06 10:15:47 -0700

    modify ActionTypes
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,4 +1,4 @@
 export const ADD_PROFILE = 'ADD_PROFILE';
-export const TOGGLE_FORM = 'TOGGLE_FORM';
 export const DELETE_PROFILE = 'DELETE_PROFILE';
+export const TOGGLE_FORM = 'TOGGLE_FORM';
 // export const SELECT_PROFILE = 'SELECT_PROFILE';

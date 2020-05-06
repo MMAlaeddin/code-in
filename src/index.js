2c70d7f861c117789f87de899745165709174a5c
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-06 13:34:58 -0700

    add firestore reducer to root reducer
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,24 +4,29 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
-// import reducer from './reducers/barrel-list-reducer';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
 const store = createStore(rootReducer);
 
-
-store.subscribe(() =>
-  console.log(store.getState())
-);
-
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store = {store} >
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import formVisibleReducer from './form-visible-reducer';
 import profileListReducer from './profile-list-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
+
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterProfileList: profileListReducer
+  masterProfileList: profileListReducer,
+  firestore: firestoreReducer
+
 });
 
 export default rootReducer;
